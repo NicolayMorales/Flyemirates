c110e46b234466d720cc6c64f4b7d5683a53e10d
NicolayMorales <nicolaymorales@gmail.com>
2021-11-25 21:14:50 -0500

    Update class

--- src/app/modulos/seguridad/login/login.component.ts
+++ src/app/modulos/seguridad/login/login.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import * as cryptoJS from 'crypto-js';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  fgValidacion = this.fb.group({
+    correo: ['', [Validators.required, Validators.email]],
+    clave: ['', [Validators.required]]
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
